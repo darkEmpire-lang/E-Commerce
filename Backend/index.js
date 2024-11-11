50bfbcce3a3777391d906144bc4dc02660586ca3
pasindu maduranga <it22546784@my.sliit.lk>
2024-11-11 20:19:42 +0530

    check backend

--- Backend/index.js
+++ Backend/index.js
@@ -14,6 +14,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+
+app.get('/hello', (req, res) => {
+  res.json({ message: "Hello, your backend is working!" });
+});
+
 const productRoutes = require('./routes/ProductRoutes');
 app.use('/products', productRoutes);
 
