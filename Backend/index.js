80ab89df9ffaff81b3391fd6e1b08ed9c701d950
pasindu maduranga <it22546784@my.sliit.lk>
2024-11-12 12:15:17 +0530

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -33,8 +33,8 @@ if (!mongoURI) {
 }
 
 mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+ // useNewUrlParser: true,
+  //useUnifiedTopology: true,
 }).then(() => {
   console.log("MongoDB connected successfully");
 }).catch(err => {
